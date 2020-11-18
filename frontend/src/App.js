8cad8a584a3c8cc16636a4a15b4a31cfbebeabad
cyrillelauzon@gmail.com <cyrillelauzon@users.noreply.github.com>
2020-11-18 14:07:34 -0500

    React component bootstrap Hello world

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
 
+import TransactionsTable from './components/transactionsTable';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Container fluid="md">
+        <Row >
+          <Col md={"2"}></Col>
+          <Col md={"8"}>
+            <TransactionsTable />
+          </Col>
+          <Col md={"2"}></Col>
+        </Row>
+      </Container>
+      
     </div>
   );
 }
---
+++ frontend/src/components/transactionsTable.jsx
@@ -0,0 +1,15 @@
+/*-------------------------------------------------------------------------
+Component   transactionsTable
+Description: 
+Display of a table of bank transactions objects
+-------------------------------------------------------------------------*/
+import React, { Component } from 'react';
+
+class TransactionsTable extends Component {
+    state = {  }
+    render() { 
+        return <h1>Hello world transaction</h1>
+    }
+}
+ 
+export default TransactionsTable;
